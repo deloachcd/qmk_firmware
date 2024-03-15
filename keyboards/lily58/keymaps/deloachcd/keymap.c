eab669f5fd8b22c1d56bef60c9c534a1370f212f
Chandler DeLoach <deloach.cdd@gmail.com>
2024-03-14 19:11:15 -0600

    get my layout building with current API

--- keyboards/lily58/keymaps/deloachcd/keymap.c
+++ keyboards/lily58/keymaps/deloachcd/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   _______, KC_F11,  KC_F12,  _______, _______, _______,                   KC_DEL,  KC_INS,  KC_PGDN, KC_PGUP, KC_PSCR, _______, \
   _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, _______, \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_NLCK, KC_CAPS, _______, _______, KC_END,  _______, \
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_NUM,  KC_CAPS, _______, _______, KC_END,  _______, \
                              _______, _______, _______, _______, _______, _______, _______, _______\
 ),
 /* ADJUST
@@ -109,31 +109,30 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 // When you add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
 const char *read_logo(void);
 void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keyboard_state(void);
-void oled_write_numlock_state(void);
-void change_numlock_oled_state(void);
-void oled_write_oldschool_logo(void);
-void oled_write_cheatsheets(void);
+const char *read_keylog(void);
+const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
 // void set_timelog(void);
 // const char *read_timelog(void);
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
-    //oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keyboard_state(), false);
-    oled_write_ln("", false);
-    oled_write_numlock_state();
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
+    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    //oled_write_ln(read_host_led_state(), false);
+    //oled_write_ln(read_timelog(), false);
   } else {
-    oled_write_cheatsheets();
-    // Default logo:
-    //oled_write(read_logo(), false);
+    oled_write(read_logo(), false);
   }
+    return false;
 }
 #endif // OLED_ENABLE
 
@@ -141,9 +140,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_ENABLE
     set_keylog(keycode, record);
-    if (keycode == KC_NLCK) {
-      change_numlock_oled_state();
-    }
 #endif
     // set_timelog();
   }
