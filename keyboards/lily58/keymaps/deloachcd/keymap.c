e85d980f762f4a6161b387ba8a6b89815b1fbb92
Chandler DeLoach <deloach.cdd@gmail.com>
2024-03-14 18:56:21 -0600

    commit my own layout

---
+++ keyboards/lily58/keymaps/deloachcd/keymap.c
@@ -0,0 +1,151 @@
+#include QMK_KEYBOARD_H
+
+enum layer_number {
+  _QWERTY = 0,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |BackSP|
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |Enter |
+ * |------+------+------+------+------+------| LGUI  |    | RGUI  |------+------+------+------+------+------|
+ * |Shift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shift |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |LCtrl | LAlt |LOWER | /Space  /       \Space \  |RAISE | RAlt |RCtrl |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+ [_QWERTY] = LAYOUT( \
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LGUI,  KC_RGUI,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                        KC_LCTL, KC_LALT, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), KC_RALT, KC_RCTL \
+),
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |   -  |   +  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   _  |   |  |   {  |   }  |   "  |                    |   *  |   7  |   8  |   9  |   -  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   =  |   \  |   (  |   )  |   '  |-------.    ,-------|   .  |   4  |   5  |   6  |   +  |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |   *  |   ~  |   [  |   ]  |   `  |-------|    |-------|   0  |   1  |   2  |   3  |   /  |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_LOWER] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, KC_PMNS, KC_PPLS, \
+  _______, KC_UNDS, KC_PIPE, KC_LCBR, KC_RCBR, KC_DQUO,                     KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, \
+  _______, KC_EQL,  KC_BSLS, KC_LPRN, KC_RPRN, KC_QUOT,                     KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, \
+  _______, KC_ASTR, KC_TILD, KC_LBRC, KC_RBRC, KC_GRV,   _______, _______,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PSLS, _______, \
+                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
+),
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | F11  | F12  |      |      |      |                    |  Del |  Ins | PgDn | PgUp |PrtSrc|      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |   ^  |   &  |   *  |-------.    ,-------| Left | Down |  Up  | Right| Home |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |   !  |   @  |   #  |   $  |   %  |-------|    |-------| NumL | Caps |      |      | End  |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_RAISE] = LAYOUT( \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+  _______, KC_F11,  KC_F12,  _______, _______, _______,                   KC_DEL,  KC_INS,  KC_PGDN, KC_PGUP, KC_PSCR, _______, \
+  _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, _______, \
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_NLCK, KC_CAPS, _______, _______, KC_END,  _______, \
+                             _______, _______, _______, _______, _______, _______, _______, _______\
+),
+/* ADJUST
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_ADJUST] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  )
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
+}
+
+// When you add source files to SRC in rules.mk, you can use functions.
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keyboard_state(void);
+void oled_write_numlock_state(void);
+void change_numlock_oled_state(void);
+void oled_write_oldschool_logo(void);
+void oled_write_cheatsheets(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // If you want to change the display of OLED, you need to change here
+    //oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keyboard_state(), false);
+    oled_write_ln("", false);
+    oled_write_numlock_state();
+  } else {
+    oled_write_cheatsheets();
+    // Default logo:
+    //oled_write(read_logo(), false);
+  }
+}
+#endif // OLED_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef OLED_ENABLE
+    set_keylog(keycode, record);
+    if (keycode == KC_NLCK) {
+      change_numlock_oled_state();
+    }
+#endif
+    // set_timelog();
+  }
+  return true;
+}
